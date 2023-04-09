f3cf0b4447cb7faaf843b67a72839d90b2df78b4
AndrewLockVI <andrewlaack1@gmail.com>
2023-04-09 14:10:17 -0500

    Missing number using vector

---
+++ missing-number/missing-number.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int missingNumber(vector<int>& nums) {
+
+    vector <int> num_seq(nums.size() + 1 , 0) ;
+    for(int i = 0 ; i < nums.size() ; ++i){
+        num_seq[nums[i]] = 1;
+    }
+    for(int i = 0 ; i < num_seq.size() ; ++i){
+        if(num_seq[i] == 0){
+            return i;
+        }
+    }
+    return 0;
+
+}
+
+
+
+
+int main(){
+
+    vector <int> search_vec = {0, 1 , 2 ,3 ,4 ,6 , 7};
+    cout << missingNumber(search_vec) << " is the missing number from the list" <<  endl;
+
+}
+
+
+
+
+
