1b4d966bb21f22db7d514e8401e8427282c6333b
AndrewLockVI <andrewlaack1@gmail.com>
2023-05-12 13:48:36 -0500

    Completed fibonacci number problem using C++

---
+++ fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,13 @@
+//Find the nth value in the fibonacci number sequence.
+//I did this recursively with a time complexity of O(n).
+//Time: 11ms Beats: 47.1%
+//Memory: 5.9MB Beats: 94.32%
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1){
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+};
