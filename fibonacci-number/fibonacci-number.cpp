132647c453f6fdf3ea81d7bbbffc71d62dc14483
AndrewLockVI <andrewlaack1@gmail.com>
2023-05-12 13:56:29 -0500

    Updated time complexity of code

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,5 +1,5 @@
 //Find the nth value in the fibonacci number sequence.
-//I did this recursively with a time complexity of O(n).
+//I did this recursively with a time complexity of O(2^n).
 //Time: 11ms Beats: 47.1%
 //Memory: 5.9MB Beats: 94.32%
 class Solution {
